3282dda3ff53150892691f671bbfd72437d94b51
Jaka Hudoklin <jaka@x-truder.net>
2021-03-24 08:33:26 +0000

    flake: add crd2pulumi and k8split tools for nix shell

--- flake.nix
+++ flake.nix
@@ -8,6 +8,26 @@
     outputs = { self, nixpkgs }: let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      crd2pulumi = pkgs.buildGoModule rec {
+        pname = "crd2pulumi";
+        version = "v1.0.5";
+        src = pkgs.fetchurl {
+          url = "https://github.com/pulumi/crd2pulumi/archive/refs/tags/${version}.tar.gz";
+          sha256 = "sha256-RkRCO8brf82UDPPyF+ms105SYBpkBLMGB/zOeuBOcFg=";
+        };
+        vendorSha256 = "sha256-eoWKtF/jzY8/2x94KKEsRoMQC6H+04n6Rj4wVkigNpg=";
+      };
+
+      k8split = pkgs.buildGoModule rec {
+        pname = "k8split";
+        version = "7469c282";
+        src = pkgs.fetchurl {
+          url = "https://github.com/brendanjryan/k8split/archive/7469c282.tar.gz";
+          sha256 = "sha256-dsuQcpy686btgfnOi6tPdoyRVljtzhrA1rHfAmA2HTY=";
+        };
+        vendorSha256 = "sha256-L0CdTy6dIpamnnNech078NWApafoVjjjyM83Cv5lbUo=";
+      };
     in {
       devShell.${system} = pkgs.mkShell {
         buildInputs = with pkgs; [
@@ -29,6 +49,8 @@
             };
           }))
           netcat
+          crd2pulumi
+          k8split
         ];
 
         shellHook = ''
